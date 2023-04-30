3b4e4d14b2efcf0194a2eb29e12cf8c97ef75f5f
Mohammed Alrabai <m.alrabai4@gmail.com>
2023-04-30 14:14:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/name", (req, res) => {
-  res.send("Mohammed Alrabai");
+  res.send("Mohammed");
 });
 
 app.listen(8000, () => {
