5bf04d54e897d15b58be238f97890db8eaf7787f
Mohammed Alrabai <m.alrabai4@gmail.com>
2023-05-01 13:48:43 +0300

    app ejs

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const app = express();
-
+app.set("view engine", "ejs");
 app.get("/hello", (req, res) => {
   res.send("Mohammed Alrabai");
+});
+app.get("/index", (req, res) => {
+  const grand = 95;
+  res.render("index.ejs", { degree: grand });
 });
 
 app.listen(8000, () => {
